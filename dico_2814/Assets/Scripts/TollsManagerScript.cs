7c98f2da3358d80f4b6ebb5e74399a91071e7a04
Théo Battello <battello.theo@gmail.com>
2018-05-14 14:17:23 +0200

    Basic game without assets

---
+++ dico_2814/Assets/Scripts/TollsManagerScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TollsManagerScript : MonoBehaviour {
+    
+    private GameObject left_toll;
+    private GameObject right_toll;
+
+	// Use this for initialization
+	void Start () {
+        left_toll = GameObject.FindWithTag("left_toll");
+        right_toll = GameObject.FindWithTag("right_toll");
+
+        left_toll.GetComponent<Renderer>().enabled = false;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            left_toll.GetComponent<Renderer>().enabled = !left_toll.GetComponent<Renderer>().enabled;
+            right_toll.GetComponent<Renderer>().enabled = !right_toll.GetComponent<Renderer>().enabled;
+        }
+    }
+}
---
+++ dico_2814/Assets/Scripts/WordSpawnerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordSpawnerScript : MonoBehaviour {
+
+    public GameObject word;
+
+    private float[] wordPositions = {-6.5f, -3.5f, 3.5f, 6.5f};
+
+    void Awake()
+    {
+        InvokeRepeating("SpawnNext", 0.5f, 0.5f);
+    }
+
+    void SpawnNext()
+    {
+        GameObject newWord = Instantiate(word);
+        int random = Random.Range(0, 4);
+        Debug.Log(random);
+        newWord.transform.position = new Vector3(wordPositions[random], 5.5f, 0);
+    }
+}
